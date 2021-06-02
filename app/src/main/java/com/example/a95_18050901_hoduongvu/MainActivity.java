ef100088f3d67158c0331d0bbf7d80ef68dd8dad
Eatmsts <deseatmsts@gmail.com>
2021-06-02 08:24:04 +0700

    Xong layout manager

--- app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
+++ app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private EditText email, pass;
     private Button login;
     private FirebaseAuth fAuth;
+    private TextView exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        exit=findViewById(R.id.txtExit);
         fAuth=FirebaseAuth.getInstance();
         email=findViewById(R.id.txtEmail);
         pass=findViewById(R.id.txtPassword);
@@ -48,14 +51,21 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(getApplicationContext(),"Đăng nhập thành công!",Toast.LENGTH_SHORT).show();
-//                            Intent i = new Intent(MainActivity.this,SingIn.class);
-//                            startActivity(i);
+                            Intent i = new Intent(MainActivity.this,Manager.class);
+                            startActivity(i);
                         } else {
                             Toast.makeText(getApplicationContext(),"Đăng nhập không thành công!",Toast.LENGTH_SHORT).show();
                             return;
                         }
                     }
                 });
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
             }
         });
     }
