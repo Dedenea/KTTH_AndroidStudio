6cd191a25ded7fcf1963da325c71546ae233b54e
Eatmsts <deseatmsts@gmail.com>
2021-06-02 07:32:17 +0700

    Xong màn hình Login

---
+++ app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a95_18050901_hoduongvu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
