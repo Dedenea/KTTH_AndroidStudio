8d8dfcb6ebd3926ec3f1b52e433ee9e506489d9e
Eatmsts <deseatmsts@gmail.com>
2021-06-02 07:58:42 +0700

    Xong login voi them commit lai man hinh loin do nay em quen cái exit

--- app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
+++ app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.a95_18050901_hoduongvu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText email, pass;
+    private Button login;
+    private FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fAuth=FirebaseAuth.getInstance();
+        email=findViewById(R.id.txtEmail);
+        pass=findViewById(R.id.txtPassword);
+        login=findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String e,p;
+                e=email.getText().toString();
+                p=pass.getText().toString();
+                if(TextUtils.isEmpty(e)){
+                    Toast.makeText(getApplicationContext(),"Chưa nhập mật Email!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(p)){
+                    Toast.makeText(getApplicationContext(),"Chưa nhập mật Password!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                fAuth.signInWithEmailAndPassword(e,p).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Đăng nhập thành công!",Toast.LENGTH_SHORT).show();
+//                            Intent i = new Intent(MainActivity.this,SingIn.class);
+//                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Đăng nhập không thành công!",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+            }
+        });
     }
 }
