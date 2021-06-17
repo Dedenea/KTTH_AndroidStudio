276446b49535d39dfc28a1393d172f477411e84a
Eatmsts <deseatmsts@gmail.com>
2021-06-17 22:04:26 +0700

    TH

--- app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
+++ app/src/main/java/com/example/a95_18050901_hoduongvu/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
                 System.exit(0);
             }
         });
